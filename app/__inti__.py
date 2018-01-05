497854deafcefd915adcbb9c509df38a41ce6f58
Kipngetich33 <khalifngeno@gmail.com>
2018-01-05 04:52:33 -0500

    import the configurations from the config.py in the application factory and create them in the create_app function

--- app/__inti__.py
+++ app/__inti__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from config import config_options
+
 
 def create_app():
     app = Flask(__name__)
+
+    #creating app configurations
+    app.config.from_object(config_options[config_name]) 
+
+    return app
 
